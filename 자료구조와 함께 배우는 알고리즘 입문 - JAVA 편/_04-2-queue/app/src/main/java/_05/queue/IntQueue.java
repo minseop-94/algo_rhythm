9d2ad4c03dab2ac880aa5257ab9e6d0bc2257494
DESKTOP-NEM60RJ\cogit <jaesme2530@gmail.com>
2022-12-26 22:09:01 +0900

    [MOD] 폴더 이름 변경 04-1 스택, 04-2 큐

--- 자료구조와 함께 배우는 알고리즘 입문 - JAVA 편/_04-2-queue/app/src/main/java/_05/queue/IntQueue.java
+++ 자료구조와 함께 배우는 알고리즘 입문 - JAVA 편/_04-2-queue/app/src/main/java/_05/queue/IntQueue.java
@@ -7,6 +7,6 @@ public class IntQueue {
     private int rear; // inQueue한 데이터 가운데 맨 뒤에 넣은 요소의 인덱스를 저장하는 필드
     private int num; // frint, rear 값이 같을 때, 큐가 비어 있는지, 가득 찼는지 구별할 수 없는 상황을 피하기 위한 변수
 
-
+    
 
 }
